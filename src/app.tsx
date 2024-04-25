89deeebdf2844ed6b977a3a947701481aa5affe5
PalmDevs <git@palmdevs.me>
2024-04-25 19:03:19 +0700

    feat: add `ErrorBoundary`

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,29 @@
 import { MetaProvider } from '@solidjs/meta'
 import { Router } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start/router'
-import { type Component, Suspense } from 'solid-js'
+import { type Component, Suspense, ErrorBoundary } from 'solid-js'
 
 import { ThemeProvider } from './contexts'
+import ErrorPage from './error-page'
 
 // Non-critical stylesheets
 import './styles/layers.scss'
 
 const App: Component = () => {
     return (
-        <Router
-            root={props => (
-                <MetaProvider>
-                    <ThemeProvider>
-                        <Suspense>{props.children}</Suspense>
-                    </ThemeProvider>
-                </MetaProvider>
-            )}
-        >
-            <FileRoutes />
-        </Router>
+        <ErrorBoundary fallback={(err, reset) => <ErrorPage error={err} reset={reset} />}>
+            <Router
+                root={props => (
+                    <MetaProvider>
+                        <ThemeProvider>
+                            <Suspense>{props.children}</Suspense>
+                        </ThemeProvider>
+                    </MetaProvider>
+                )}
+            >
+                <FileRoutes />
+            </Router>
+        </ErrorBoundary>
     )
 }
 
---
+++ src/error-page.tsx
@@ -0,0 +1,30 @@
+import type { Component } from 'solid-js'
+import { RepositoryLinks } from './constants/links'
+
+const ErrorPage: Component<{ error: unknown; reset: () => void }> = props => {
+    return (
+        <main id="error-page">
+            <h1>Something went very wrong</h1>
+            <p>
+                Try refreshing the page. If that still doesn't work, you can report an issue{' '}
+                <a target="_blank" rel="noreferrer" style="color: var(--primary)" href={RepositoryLinks.issues}>here</a>.
+            </p>
+            <h4>Stack trace (include this in the bug report):</h4>
+            <code id="error-page__stack">{String(props.error)}</code>
+            <div id="error-page__buttons">
+                <button
+                    type="button"
+                    onClick={() => location.reload()}
+                    style="background: var(--primary); color: var(--on-primary)"
+                >
+                    Reload
+                </button>
+                <button type="button" onClick={props.reset} style="background: var(--secondary); color: var(--on-secondary)">
+                    Try a re-render (won't reload the site)
+                </button>
+            </div>
+        </main>
+    )
+}
+
+export default ErrorPage
