57aa8f5392e2cd088c7906c1e30706a3b67a7076
PalmDevs <git@palmdevs.me>
2024-06-16 20:51:51 +0700

    feat(components): abstract `ProjectCard` into `LinkCard`

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,40 +1,20 @@
-import { Column, Row } from './Page'
-import Touchable from './Touchable'
-
-import IconNext from '~/assets/icons/next.svg'
-
 import type { Component } from 'solid-js'
+
+import LinkCard from './LinkCard'
+import { Column } from './Page'
 
 import styles from './ProjectCard.module.scss'
 
 const ProjectCard: Component<ProjectCardProps> = props => {
     return (
-        <Touchable
-            class={styles.Card}
-            aria-label={`${props.name}. ${props.description} Tap to ${props.hint}.`}
-            flex
-            as={Column}
-            asProps={{
-                gap: 'none',
-                as: 'a',
-                target: '_blank',
-                rel: 'noreferrer',
-                href: props.href,
-            }}
-        >
-            <Column aria-hidden="true" centerHorizontal centerVertical class={styles.ImageContainer}>
-                <img draggable="false" loading="lazy" src={props.image} alt={props.name} />
-            </Column>
-            <Column flex gap="md" class={styles.InfoContainer}>
-                <Column gap="none">
-                    <h3>{props.name}</h3>
-                    <p style="margin: 0">{props.description}</p>
+        <LinkCard
+            {...props}
+            preview={() => (
+                <Column aria-hidden="true" centerHorizontal centerVertical class={styles.ImageContainer}>
+                    <img draggable="false" loading="lazy" src={props.image} alt={props.name} />
                 </Column>
-                <Row as="p" gap="xs" centerVertical class={styles.Hint} aria-hidden="true">
-                    {props.hint ?? 'Learn more'} <IconNext />
-                </Row>
-            </Column>
-        </Touchable>
+            )}
+        />
     )
 }
 
