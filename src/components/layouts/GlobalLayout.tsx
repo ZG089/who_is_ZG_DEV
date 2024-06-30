07354a3e61ba03b471c739e41ca229509b638dc8
PalmDevs <git@palmdevs.me>
2024-06-30 23:19:43 +0700

    fix: `BottomBar` mounting issues, style issues, code refactor

--- src/components/layouts/GlobalLayout.tsx
+++ src/components/layouts/GlobalLayout.tsx
@@ -1,12 +1,51 @@
-import { type Component, type JSX, Suspense } from 'solid-js'
-import { NavDock } from '~/components'
+import { type Component, type JSX, Suspense, useContext, Show, onMount, onCleanup, createSignal } from 'solid-js'
+import JSConfetti from 'js-confetti'
+import { format } from 'timeago.js'
+
+import { NavDock, BottomBanner, Row, Button } from '~/components'
+import { Birthday, BirthdayEnd } from '~/constants/personal'
+import { ThemeContext } from '~/contexts'
 
 import IconBlog from '~/assets/icons/nav/blog.svg'
 import IconHome from '~/assets/icons/nav/home.svg'
 import IconSource from '~/assets/icons/source.svg'
 
+import sharedStyles from '~/styles/shared.module.scss'
+
 const GlobalLayout: Component<{ children: JSX.Element }> = props => {
+    const [time, setTime] = createSignal<string | null>('...')
+    const theme = useContext(ThemeContext)
+
     let canvasRef: HTMLCanvasElement | undefined
+    let confetti: JSConfetti | undefined
+
+    const launchConfetti = () => {
+        confetti?.addConfetti({
+            confettiRadius: 4,
+            confettiNumber: 250,
+            confettiColors: theme.colorScheme === 'light' ? ['#29a873', '#007cfa'] : ['#4feaa9', '#57b7d9'],
+        })
+    }
+
+    onMount(() => {
+        confetti = new JSConfetti({ canvas: document.getElementById('confetti-canvas') as HTMLCanvasElement })
+
+        const fn = () => {
+            if (Date.now() >= Birthday.getTime()) {
+                launchConfetti()
+                setTime(null)
+                clearInterval(interval)
+                return
+            }
+
+            setTime(format(Birthday, 'countdown'))
+        }
+
+        fn()
+        const interval = setInterval(() => fn, 1000)
+
+        onCleanup(() => clearInterval(interval))
+    })
 
     return (
         <>
@@ -29,6 +68,25 @@ const GlobalLayout: Component<{ children: JSX.Element }> = props => {
                 ]}
             />
             <Suspense>{props.children}</Suspense>
+            <Show when={Date.now() < BirthdayEnd.getTime()}>
+                <BottomBanner id="2024-bd" closeLabel="Close" onClose={launchConfetti}>
+                    <Row gap="sm" flex wrap centerVertical style="justify-content: space-between">
+                        <p style="margin: 0">
+                            <Show
+                                when={time()}
+                                fallback={
+                                    <>
+                                        Today's my <span class={sharedStyles.GradientText}>birthday!</span> 🎂
+                                    </>
+                                }
+                            >
+                                It's <span class={sharedStyles.GradientText}>{time()}</span> before my birthday! 🎂
+                            </Show>
+                        </p>
+                        <Button onClick={launchConfetti}>Launch confetti 🎉</Button>
+                    </Row>
+                </BottomBanner>
+            </Show>
         </>
     )
 }
---
+++ src/constants/personal.ts
@@ -0,0 +1,22 @@
+import { register } from 'timeago.js'
+
+const year = new Date().getUTCFullYear()
+
+export const Birthday = new Date(`${year}-07-01 00:00:00 UTC+7`)
+export const BirthdayEnd = new Date(`${year}-07-02 00:00:00 UTC+7`)
+export const BirthdayLocale = 'birthday-locale'
+
+register(
+    BirthdayLocale,
+    (_: number, index: number, __?: number): [string, string] =>
+        (
+            [
+                ['a few seconds', 'a few seconds'],
+                ['%s seconds', '%s seconds'],
+                ['1 minute', '1 minute'],
+                ['%s minutes', '%s minutes'],
+                ['1 hour', '1 hour'],
+                ['%s hours', '%s hours'],
+            ] as [string, string][]
+        )[index],
+)
