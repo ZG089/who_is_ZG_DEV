fe3ae379245fd408e02e9dc53facf30f3ca71368
PalmDevs <git@palmdevs.me>
2024-06-30 22:51:51 +0700

    fix: mount bottom bar for birthday in `Page` instead

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,15 +1,81 @@
-import { type Component, type ComponentProps, type JSX, mergeProps, splitProps, Show } from 'solid-js'
-import { Dynamic } from 'solid-js/web'
-
-import { combineClassNames, undefinedIf } from '~/utils'
-
-import sharedStyles from '~/styles/shared.module.scss'
-import styles from './Page.module.scss'
+import JSConfetti from 'js-confetti'
 import { Link, Meta } from '@solidjs/meta'
 import { useLocation } from '@solidjs/router'
+import {
+    type Component,
+    type ComponentProps,
+    type JSX,
+    mergeProps,
+    splitProps,
+    createSignal,
+    onMount,
+    onCleanup,
+    Show,
+    useContext,
+} from 'solid-js'
+import { Dynamic } from 'solid-js/web'
+import { format, register } from 'timeago.js'
 
-export const Page: Component<ComponentProps<'main'> & { noCrawl?: boolean; noSetCanonical?: boolean; }> = props => {
+import { combineClassNames, undefinedIf } from '~/utils'
+import { BottomBanner, Button } from '~/components'
+import { ThemeContext } from '~/contexts'
+
+import styles from './Page.module.scss'
+import sharedStyles from '~/styles/shared.module.scss'
+
+const year = new Date().getUTCFullYear()
+const birthday = new Date(`${year}-07-01 00:00:00 UTC+7`)
+const birthdayEnd = new Date(`${year}-07-02 00:00:00 UTC+7`)
+
+register(
+    'countdown',
+    (_: number, index: number, __?: number): [string, string] =>
+        (
+            [
+                ['a few seconds', 'a few seconds'],
+                ['%s seconds', '%s seconds'],
+                ['1 minute', '1 minute'],
+                ['%s minutes', '%s minutes'],
+                ['1 hour', '1 hour'],
+                ['%s hours', '%s hours'],
+            ] as [string, string][]
+        )[index],
+)
+
+export const Page: Component<ComponentProps<'main'> & { noCrawl?: boolean; noSetCanonical?: boolean }> = props => {
+    const theme = useContext(ThemeContext)
     const loc = useLocation()
+    const [time, setTime] = createSignal<string | null>('...')
+
+    let confetti: JSConfetti | undefined
+
+    const launchConfetti = () => {
+        confetti?.addConfetti({
+            confettiRadius: 4,
+            confettiNumber: 250,
+            confettiColors: theme.colorScheme === 'light' ? ['#29a873', '#007cfa'] : ['#4feaa9', '#57b7d9'],
+        })
+    }
+
+    onMount(() => {
+        confetti = new JSConfetti({ canvas: document.getElementById('confetti-canvas') as HTMLCanvasElement })
+
+        const fn = () => {
+            if (Date.now() >= birthday.getTime()) {
+                launchConfetti()
+                setTime(null)
+                clearInterval(interval)
+                return
+            }
+
+            setTime(format(birthday, 'countdown'))
+        }
+
+        fn()
+        const interval = setInterval(() => fn, 1000)
+
+        onCleanup(() => clearInterval(interval))
+    })
 
     return (
         <Column as="main" flex centerHorizontal tabIndex="-1">
@@ -22,6 +88,25 @@ export const Page: Component<ComponentProps<'main'> & { noCrawl?: boolean; noSet
             <Column {...props} centerHorizontal class={combineClassNames(props.class, styles.Content)}>
                 {props.children}
             </Column>
+            <Show when={Date.now() < birthdayEnd.getTime()}>
+                <BottomBanner id="2024-bd" closeLabel="Close" onClose={launchConfetti}>
+                    <Row gap="sm" flex wrap centerVertical style="justify-content: space-between">
+                        <p style="margin: 0">
+                            <Show
+                                when={time()}
+                                fallback={
+                                    <>
+                                        Today's my <span class={sharedStyles.GradientText}>birthday!</span> 🎂
+                                    </>
+                                }
+                            >
+                                It's <span class={sharedStyles.GradientText}>{time()}</span> before my birthday! 🎂
+                            </Show>
+                        </p>
+                        <Button onClick={launchConfetti}>Launch confetti 🎉</Button>
+                    </Row>
+                </BottomBanner>
+            </Show>
         </Column>
     )
 }
--- src/components/layouts/GlobalLayout.tsx
+++ src/components/layouts/GlobalLayout.tsx
@@ -1,71 +1,17 @@
-import { type Component, type JSX, Suspense, createSignal, onMount, onCleanup, Show, useContext } from 'solid-js'
-import NavDock from '~/components/NavDock'
-import BottomBanner from '~/components/BottomBanner'
-
-import JSConfetti from 'js-confetti'
-import { format, register } from 'timeago.js'
+import { type Component, type JSX, Suspense } from 'solid-js'
+import { NavDock } from '~/components'
 
 import IconBlog from '~/assets/icons/nav/blog.svg'
 import IconHome from '~/assets/icons/nav/home.svg'
 import IconSource from '~/assets/icons/source.svg'
 
-import sharedStyles from '~/styles/shared.module.scss'
-import { ThemeContext } from '~/contexts'
-
-const year = new Date().getUTCFullYear()
-const birthday = new Date(`${year}-07-01 00:00:00 UTC+7`)
-const birthdayEnd = new Date(`${year}-07-02 00:00:00 UTC+7`)
-
-register(
-    'countdown',
-    (_: number, index: number, __?: number): [string, string] =>
-        (
-            [
-                ['a few seconds', 'a few seconds'],
-                ['%s seconds', '%s seconds'],
-                ['1 minute', '1 minute'],
-                ['%s minutes', '%s minutes'],
-                ['1 hour', '1 hour'],
-                ['%s hours', '%s hours'],
-            ] as [string, string][]
-        )[index],
-)
-
 const GlobalLayout: Component<{ children: JSX.Element }> = props => {
-    const theme = useContext(ThemeContext)
-    const [time, setTime] = createSignal<string | null>('...')
-
     let canvasRef: HTMLCanvasElement | undefined
-    let confetti: JSConfetti | undefined
-
-    const launchConfetti = () => {
-        confetti?.addConfetti({
-            confettiRadius: 4,
-            confettiNumber: 250,
-            confettiColors: theme.colorScheme === 'light' ? ['#29a873', '#007cfa'] : ['#4feaa9', '#57b7d9'],
-        })
-    }
-
-    onMount(() => {
-        confetti = new JSConfetti({ canvas: canvasRef })
-
-        const interval = setInterval(() => {
-            if (Date.now() >= birthday.getTime()) {
-                launchConfetti()
-                setTime(null)
-                clearInterval(interval)
-                return
-            }
-
-            setTime(format(birthday, 'countdown'))
-        }, 1000)
-
-        onCleanup(() => clearInterval(interval))
-    })
 
     return (
         <>
             <canvas
+                id="confetti-canvas"
                 ref={canvasRef}
                 style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; pointer-events: none; z-index: var(--layer-overlay);"
             />
@@ -83,22 +29,6 @@ const GlobalLayout: Component<{ children: JSX.Element }> = props => {
                 ]}
             />
             <Suspense>{props.children}</Suspense>
-            <Show when={Date.now() < birthdayEnd.getTime()}>
-                <BottomBanner id="2024-bd" closeLabel="Happy birthday!" onClose={launchConfetti}>
-                    <div>
-                        <Show
-                            when={time()}
-                            fallback={
-                                <>
-                                    Today's my <span class={sharedStyles.GradientText}>birthday!</span> 🎂
-                                </>
-                            }
-                        >
-                            It's <span class={sharedStyles.GradientText}>{time()}</span> before my birthday! 🎂
-                        </Show>
-                    </div>
-                </BottomBanner>
-            </Show>
         </>
     )
 }
